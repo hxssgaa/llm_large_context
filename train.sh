871dbd0b78e9933b86687555118044985d618d24
David Huang <hxssg1124@gmail.com>
2023-12-11 17:09:33 +0800

    fix

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ python3 -m llamabpt.train \
     --save_model_freq=0 \
     --save_milestone_freq=1000 \
     --load_llama_config='7b' \
-    --update_llama_config="dict(max_sequence_length=524288,scan_attention=True,scan_query_chunk_size=2048,scan_key_chunk_size=4096,remat_attention='nothing_saveable',scan_mlp=True,scan_mlp_chunk_size=2048,remat_mlp='nothing_saveable',remat_block='nothing_saveable',scan_layers=True,attention_type='ring_blockwise',param_scan_axis=0,mesh_dim='1,1,4,8')" \
+    --update_llama_config="dict(max_sequence_length=131072,scan_attention=True,scan_query_chunk_size=2048,scan_key_chunk_size=4096,remat_attention='nothing_saveable',scan_mlp=True,scan_mlp_chunk_size=2048,remat_mlp='nothing_saveable',remat_block='nothing_saveable',scan_layers=True,attention_type='ring_blockwise',param_scan_axis=0,mesh_dim='1,1,4,8')" \
     --load_dataset_state='' \
     --load_checkpoint='' \
     --tokenizer.vocab_file="gs://hxtpu_bucket/llama2_tokenizer.model" \
@@ -22,6 +22,6 @@ python3 -m llamabpt.train \
     --train_dataset.huggingface_dataset.name='en' \
     --train_dataset.huggingface_dataset.split='train' \
     --train_dataset.huggingface_dataset.streaming=True \
-    --train_dataset.huggingface_dataset.seq_length=524288 \
+    --train_dataset.huggingface_dataset.seq_length=131072 \
     --train_dataset.huggingface_dataset.batch_size=1 \
     --checkpointer.save_optimizer_state=True
